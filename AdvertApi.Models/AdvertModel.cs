bd21433bad43fe7464abe8c437b6b374faa38956
John Cassidy <jpcassidy@outlook.com>
2022-05-02 08:56:26 -0400

    Add project files.

---
+++ AdvertApi.Models/AdvertModel.cs
@@ -0,0 +1,8 @@
+﻿namespace AdvertApi.Models {
+    public class AdvertModel
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public double Price { get; set; }
+    }
+}
---
+++ AdvertApi.Models/AdvertStatus.cs
@@ -0,0 +1,6 @@
+﻿namespace AdvertApi.Models {
+    public enum AdvertStatus {
+        Pending = 1,
+        Active = 2
+    }
+}
